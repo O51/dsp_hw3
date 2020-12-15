09e3196d395b6e2911a1b54d841ff1f32528303a
O51 <75664567+O51@users.noreply.github.com>
2020-12-15 18:04:12 +0800

    Add files via upload

--- mapping.py
+++ mapping.py
@@ -5,7 +5,7 @@ class mapping:
 		self.output = To
 		self.map = dict()
 	def read_map():
-		input_map = open(self.input,encoding = "Big5",'r')
+		input_map = open(self.input,'r',encoding = "Big5")
 		line = input_map.readlines()
 		input_map.close()
 		for ln in line:
@@ -19,7 +19,7 @@ class mapping:
 		outlist = []
 		for key in keylist:
 			outlist.append(key+" "+" ".join(self.map[key])+"\n")
-		output_dump = open(self.output,encoding = "Big5",'w')
+		output_dump = open(self.output,'w',encoding = "Big5")
 		output_dump.writelines(outlist)
 		output_dump.close()
 if __name__ == '__main__':
